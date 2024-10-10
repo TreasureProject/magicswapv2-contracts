a06ea01c4f967ee04a9009546c3d1072d635a62b
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-10-10 09:45:10 -0700

    update deploy script

--- sh/deployArbitrumSepolia.sh
+++ sh/deployArbitrumSepolia.sh
@@ -3,3 +3,4 @@ source .env
 
 # To deploy and verify our contract
 forge script script/MagicswapV2.s.sol:MagicswapV2Script --aws --rpc-url $ARBITRUM_SEPOLIA_RPC --broadcast --verify -vvvv
+forge script script/StakingContract.s.sol:StakingContractScript --aws --rpc-url $ARBITRUM_SEPOLIA_RPC --broadcast --verify -vvvv
