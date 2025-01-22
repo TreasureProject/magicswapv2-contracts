cc745f6f88380807eea0f002c63374a70cd92301
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2025-01-21 20:00:07 -0800

    fix zksolc version; update treasure mainnet deployment artifacts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -61,7 +61,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.5.7",
+    version: "1.5.4",
     settings: {
       // find all available options in the official documentation
       // https://docs.zksync.io/build/tooling/hardhat/hardhat-zksync-solc#configuration
